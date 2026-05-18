0f2d84fcc80b957c240def30787279a5563c5ad9
vertraxumtesOG <kekfluffy@gmail.com>
2026-05-19 01:40:18 +0200

    Revert "feat: enhance time log modal with closing animations and state management"
    This reverts commit f40821438bc836e5f0f47f92d4431684b4f99746.

--- src/app/components/time-log/time-log-modal/time-log-modal.component.ts
+++ src/app/components/time-log/time-log-modal/time-log-modal.component.ts
@@ -24,8 +24,6 @@ export class TimeLogModalComponent {
   @Input() userId: number | null = null;
   @Output() close = new EventEmitter<void>();
 
-  isClosing = signal(false);
-
   selectedMonth = signal('all');
 
   filteredLogins = computed(() => {
@@ -91,24 +89,12 @@ export class TimeLogModalComponent {
 
   onClose(): void {
     this.selectedMonth.set('all');
-    // start close animation, emit close after animation completes
-    if (!this.isClosing()) {
-      this.isClosing.set(true);
-    }
+    this.close.emit();
   }
 
   onOverlayClick(event: MouseEvent): void {
     if (event.target === event.currentTarget) {
       this.onClose();
-    }
-  }
-
-  onAnimationEnd(event: AnimationEvent): void {
-    // only act when closing animation finished
-    const name = event.animationName || '';
-    if (this.isClosing() && (name === 'fadeOut' || name === 'fadeOutUp')) {
-      this.isClosing.set(false);
-      this.close.emit();
     }
   }
 }
