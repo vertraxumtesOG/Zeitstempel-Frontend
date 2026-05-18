70feb6b63638ac0c58f157bb779627c0dc4a9ae3
vertraxumtesOG <kekfluffy@gmail.com>
2026-05-19 01:25:33 +0200

    feat: add footer component with styles and integrate into app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,20 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+    <ng-container 
+      *ngComponentOutlet="FooterComponent"
+      >
+    </ng-container>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {}
+export class App {
+  FooterComponent = FooterComponent;
+}
