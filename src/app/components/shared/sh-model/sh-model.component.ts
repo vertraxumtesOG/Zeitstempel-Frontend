6a3621a3b393f32850084b7cf2ad09c351a514dd
vertraxumtesOG <kekfluffy@gmail.com>
2026-05-19 02:02:31 +0200

    style: improve code formatting for consistency and readability in modal components

--- src/app/components/shared/sh-model/sh-model.component.ts
+++ src/app/components/shared/sh-model/sh-model.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
